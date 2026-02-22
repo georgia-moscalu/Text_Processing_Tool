e97a6d5d7a86d0aa6391adb7c33ac06f2495cb7a
georgia-moscalu <georgiamoscalu@gmail.com>
2026-02-22 10:06:15 +0000

    feat: eliminate punctuation funct

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ def read_file(name):
     with open(name,'r') as f: #deschide fisierul si automat il inchide
         return f.read() #ia toate caracterele si spatiile din fisier si returneaza un sir lung de car
 
+def elim_punct(text):
+    semne='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    text_curat="".join(caracter for caracter in text if caracter not in semne)
+    #'h' 'e' 'l' 'l' 'o' join->le lipseste cu un sir vid ""
+    return text_curat
 
+#testing
 text_init=read_file('input.txt')
 print (text_init)
+text_fara_punct=elim_punct(text_init)
+print(text_fara_punct)
