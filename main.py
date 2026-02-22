90b02a6987504a746af161874cd6aa985d12e50b
georgia-moscalu <georgiamoscalu@gmail.com>
2026-02-22 10:15:24 +0000

    feat: to lower funct

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ def elim_punct(text):
     text_curat="".join(caracter for caracter in text if caracter not in semne)
     #'h' 'e' 'l' 'l' 'o' join->le lipseste cu un sir vid ""
     return text_curat
+def to_lower(text):
+    rezultat=""
+    for caracter in text:
+        cod=ord(caracter) #primeste caracter A si returneaza ord 65
+        if 65 <= cod <= 90:
+            rezultat+=chr(cod+32) #chr - de la nr la litera
+        else:
+            rezultat+=caracter
+    return rezultat
 
 #testing
 text_init=read_file('input.txt')
-print (text_init)
 text_fara_punct=elim_punct(text_init)
-print(text_fara_punct)
+text_litere_mici=to_lower(text_fara_punct)
+print(text_litere_mici)
