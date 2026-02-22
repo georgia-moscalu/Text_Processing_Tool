b3b64bf22b6456ce3df2936a149998d1fa18e12c
georgia-moscalu <georgiamoscalu@gmail.com>
2026-02-22 09:44:34 +0000

    feat: read file

---
+++ main.py
@@ -0,0 +1,7 @@
+def read_file(name):
+    with open(name,'r') as f: #deschide fisierul si automat il inchide
+        return f.read() #ia toate caracterele si spatiile din fisier si returneaza un sir lung de car
+
+
+text_init=read_file('input.txt')
+print (text_init)
